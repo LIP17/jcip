5b44c2bf48cc8c6c063b9fc8286e7eed17aa0f57
Li Pei <lipei.work@gmail.com>
2017-10-15 22:58:36 -0400

    Chapter4: Composing (#2)

---
+++ src/main/java/chapter4/JavaMonitorPattern.java
@@ -0,0 +1,40 @@
+package chapter4;
+
+import chapter2.Widget;
+import net.jcip.annotations.GuardedBy;
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * Java Monitor Pattern: encapsulate all its mutable state and
+ * guards it with the objects' own intrinsic lock, or private lock.
+ * */
+public interface JavaMonitorPattern {
+
+}
+
+class IntrinsicLockImplementation<T> implements JavaMonitorPattern {
+    @GuardedBy("this")
+    private final Set<T> set = new HashSet<T>();
+
+    public synchronized void add(T t) { set.add(t); }
+
+    public synchronized boolean contains(T t) { return set.contains(t); }
+}
+
+/**
+ * The benefit of using private lock is, other object access this will
+ * never get the intrinsic lock, in case client code doing something bad.
+ * */
+class PrivateLockImplementation implements JavaMonitorPattern {
+    private final Object internalLock = new Object();
+
+    @GuardedBy("this")
+    private Widget widget;
+
+    void someMethod() {
+        synchronized (internalLock) {
+            // access widget
+        }
+    }
+}
+
